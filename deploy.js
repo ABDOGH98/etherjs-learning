3e3f0a1df7c1ddaa8e406d923b7b8128c58f212c
unknown <abdessamad.ghachi98@gmail.com>
2023-01-01 13:09:12 +0100

    using node as a service

--- deploy.js
+++ deploy.js
@@ -8,14 +8,7 @@ async function main() {
 	const provider = new ethers.providers.JsonRpcBatchProvider(
 		process.env.RPC_URL,
 	);
-	const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
 	const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-	// let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-	// 	encryptedJson,
-	// 	process.env.PRIVATE_KEY_PASSWORD,
-	// );
-	wallet = await wallet.connect(provider);
-	console.log("Wallet --> ", wallet);
 	const abi = fs.readFileSync(
 		"./SimpleStorage_SimpleStorage_sol_SimpleStorage.abi",
 		"utf-8",
@@ -28,21 +21,6 @@ async function main() {
 	console.log("Deploying, please wait ....");
 	const contract = await contractFactory.deploy();
 	const transactionReceipt = await contract.deployTransaction.wait(1); //number of confirmation to wait = 1
-	// console.log(transactionReceipt);
-	// const nonce = await wallet.getTransactionCount();
-	// const tx = {
-	// 	nonce,
-	// 	gasPrice: 20000000000,
-	// 	gasLimit: 1000000,
-	// 	to: null,
-	// 	value: 0,
-	// 	data: "0x6080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfea26469706673582212203fed89f05b7e0235b425e03b3f112d1f6eb2f1a5f27f5eb489d757467959250064736f6c63430008110033",
-	// 	chainId: 1337,
-	// };
-	// const signedTxResponse = await wallet.signTransaction(tx);
-	// const sentTxResponse = await wallet.sendTransaction(tx);
-	// await sentTxResponse.wait(1);
-	// console.log(sentTxResponse);
 
 	//call function from contract
 	const currentFavoriteNumber1 = await contract.retrieve();
